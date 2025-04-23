d9fd5d0da9b7d4adad3f1d4e0a87ed7cdb845dfc
Afzal-Riaz <mafzalriaz85@gmail.com>
2025-04-23 20:49:31 +0500

    Update: create a new file and add new code

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('');
-});
+// Route::get('/', function () {
+//     return view('');
+// });
 
+
+Route::get('', fn() => to_route('jobs.index'));
 Route::resource('jobs', JobController::class)
 ->only(['index']);
