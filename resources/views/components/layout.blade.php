2f182309ab094747ba859e1eb638bf905b80dcc5
Afzal-Riaz <mafzalriaz85@gmail.com>
2025-04-25 20:54:51 +0500

    modifi old files and add new code and functions

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <title>Laravel Job Board</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
